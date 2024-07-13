ffa0340af3df6b1fb673b8d196ad9a41ba121834
SZ <atwahsz@gmail.com>
2024-07-13 12:14:37 +0300

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -6,7 +6,18 @@ import soundfile as sf
 import numpy as np
 from streamlit_mic_recorder import mic_recorder
 import os
-
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
@@ -24,18 +35,6 @@ hide_github_icon = """
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 
 # Load the model configuration
