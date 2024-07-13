4b7b23674d3152208dadfa567a832459cca1030e
SZ <atwahsz@gmail.com>
2024-07-13 12:10:16 +0300

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -24,6 +24,20 @@ hide_github_icon = """
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
 # Load the model configuration
 config = AutoConfig.from_pretrained("jstoone/distil-ast-audioset-finetuned-cry")
 
