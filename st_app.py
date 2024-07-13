1f75584a750ff5c9f0336656aaf371d74ba4006e
SZ <atwahsz@gmail.com>
2024-07-13 12:15:18 +0300

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -27,13 +27,7 @@ footer {visibility: hidden;}
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 
 
